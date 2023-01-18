7fce7b1ad9b03d445706901863db99d922635805
Andrey Mihaylin <look2009new@gmail.com>
2023-01-18 11:20:28 +0300

    Add domain layer.

---
+++ app/src/main/java/com/example/shoppinglist/domain/DeleteShoppingItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.shoppinglist.domain
+
+class DeleteShoppingItemUseCase {
+    fun deleteShoppingItem(shoppingItem: ShoppingItem) {
+        TODO()
+    }
+}
---
+++ app/src/main/java/com/example/shoppinglist/domain/GetShoppintItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.shoppinglist.domain
+
+class GetShoppintItemUseCase {
+    fun getShoppingItem(id: Int): ShoppingItem {
+        TODO()
+    }
+}
